899da0b24bdd8743ff609ac7b92ff284e6af6647
Shane Goonasekera <theshanukg@gmail.com>
2024-01-02 05:11:43 -0700

    websockets

--- src/index.ts
+++ src/index.ts
@@ -15,6 +15,26 @@ app.route("/user", user);
 app.route("/server", server);
 
 Bun.serve({
-  fetch: app.fetch,
   port: process.env.PORT || 4000,
+  fetch: (req, server) => {
+    if (server.upgrade(req)) {
+      console.log("help!");
+      return new Response("Hello world");
+    }
+    return app.fetch(req, server);
+  },
+  websocket: {
+    message(ws, message) {
+      console.log(ws.readyState, message);
+    }, // a message is received
+    open(ws) {
+      console.log("hell9!");
+    }, // a socket is opened
+    close(ws, code, message) {
+      console.log("Closed!");
+    }, // a socket is closed
+    drain(ws) {
+      console.log("drained!");
+    }, // the socket is ready to receive more data
+  },
 });
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,4 +1,8 @@
 import { Hono } from "hono";
+
+import z from "zod";
+import { zValidator } from "@hono/zod-validator";
+
 import { db } from "../database";
 
 const user = new Hono();
@@ -15,28 +19,40 @@ user.get("/:id", async (c) => {
 });
 
 // POST /user
-user.post("/", async (c) => {
-  const { id, username, email } = await c.req.json();
-  const user = await db.user.upsert({
-    select: {
-      id: true,
-      username: true,
-      email: true,
-      imageUrl: true,
-    },
-    where: {
-      auth: id,
-    },
-    create: {
-      auth: id,
-      username: username,
-      imageUrl: `https://api.dicebear.com/7.x/thumbs/svg?backgroundColor=ffd5dc&seed=${id}`,
-      email: email,
-    },
-    update: {},
-  });
+user.post(
+  "/",
+  zValidator(
+    "json",
+    z.object({
+      id: z.string(),
+      username: z.string(),
+      email: z.string(),
+    })
+  ),
+  async (c) => {
+    const { id, username, email } = c.req.valid("json");
+    
+    const user = await db.user.upsert({
+      select: {
+        id: true,
+        username: true,
+        email: true,
+        imageUrl: true,
+      },
+      where: {
+        auth: id,
+      },
+      create: {
+        auth: id,
+        username: username,
+        imageUrl: `https://api.dicebear.com/7.x/thumbs/png?backgroundColor=ffd5dc&seed=${id}`,
+        email: email,
+      },
+      update: {},
+    });
 
-  return c.json({ user });
-});
+    return c.json({ user });
+  }
+);
 
 export default user;
